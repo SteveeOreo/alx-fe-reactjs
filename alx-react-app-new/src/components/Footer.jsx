9a73d62c658351a02463354b663cdf3501e2e86b
Ekwueme Stephen <stephen.ekwuemec@gmail.com>
2025-07-16 02:19:14 +0100

    styles my componets with css inline syling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', color: 'orange'}}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    );
+}
+
+export default  Footer
