847f6057eb847a1aa3783ce022b5b72c05bcb2e6
Ekwueme Stephen <stephen.ekwuemec@gmail.com>
2025-07-16 02:36:04 +0100

    created a counter component that has 3 buttons

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,9 +3,8 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -17,6 +16,7 @@ function App() {
        <Header />
        <MainContent />
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography." />
+       <Counter/>
        <Footer />
     </>
   )
