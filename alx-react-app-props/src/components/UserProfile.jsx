127383b1abc36a2bc92a14ed769742bb99da6601
Ekwueme Stephen <stephen.ekwuemec@gmail.com>
2025-07-18 23:28:07 +0100

    updated

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,16 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  
-  export default UserProfile;
-  
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
+
+const UserProfile = () => {
+  const { name, age, bio } = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+      <p>Bio: {bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
