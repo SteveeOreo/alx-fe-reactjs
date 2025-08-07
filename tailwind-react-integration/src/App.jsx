3f75b1980d95738071ab5092f4d2d0493501ecf5
Ekwueme Stephen <stephen.ekwuemec@gmail.com>
2025-08-07 10:02:41 +0100

    created tailwind-react-integretion (v4.1 react app)

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
+    <>
+     
+    </>
   )
 }
+
+export default App
