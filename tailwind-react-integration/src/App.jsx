783a36d73dfcf2fbedb8318424fc346a6b640713
Ekwueme Stephen <stephen.ekwuemec@gmail.com>
2025-08-07 12:17:53 +0100

    Creadted tailwind-react-integration react app using (tailwindcss v4.1) and styled UserProfile component that was also created

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <UserProfile />
     </>
   )
 }
