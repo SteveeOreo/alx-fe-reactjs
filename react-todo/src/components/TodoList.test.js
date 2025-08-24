89f219c53426c1eb94a64948bcc020118b05f151
Ekwueme Stephen <stephen.ekwuemec@gmail.com>
2025-08-24 20:44:55 +0100

    moved TodoList.test.js to components folder

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import TodoList from "../components/TodoList";
+import TodoList from "./TodoList";
 
 describe("TodoList Component", () => {
   test("renders initial todos", () => {
